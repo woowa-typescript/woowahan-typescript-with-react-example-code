eea5485e3a060f6b2f6a0703c65c84569689ccfe
최현준 <chj878194@woowahan.com>
2023-10-23 19:56:18 +0900

    feat: 1장 코드 추가

---
+++ 1장/1.2.2-1.js
@@ -0,0 +1,6 @@
+// 이 함수는 숫자 a, b의 합을 반환한다.
+const sumNumber = (a, b) => {
+  return a + b;
+};
+
+sumNumber(1, 2); // 3
---
+++ 1장/1.2.2-2.js
@@ -0,0 +1,7 @@
+// 이 함수는 숫자 a, b의 합을 반환한다.
+const sumNumber = (a, b) => {
+  return a + b;
+};
+
+sumNumber(100); // NaN
+sumNumber("a", "b"); // ab
