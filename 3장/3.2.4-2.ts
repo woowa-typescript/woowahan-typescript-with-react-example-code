a69f2c2c7722bab524e8f7c9305b462badd547a2
redcontroller <jasinis102@gmail.com>
2024-03-26 14:58:09 +0900

    fix: 3.2.4-2 예제 코드 오류 수정

--- 3장/3.2.4-2.ts
+++ 3장/3.2.4-2.ts
@@ -1,9 +1,8 @@
 const PromotionList = [
   { type: "product", name: "chicken" },
   { type: "product", name: "pizza" },
-  { type: "card", name: "chee-up" },
+  { type: "card", name: "cheer-up" },
 ];
 
-type ElementOf<T> = (typeof T)[number];
 // type PromotionItemType = { type: string; name: string }
-type PromotionItemType = ElementOf<PromotionList>;
+type PromotionItemType = typeof PromotionList[number];
