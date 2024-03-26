d2b3a756fc6407992cad19909987954b5d1f48ca
redcontroller <jasinis102@gmail.com>
2024-03-26 14:51:33 +0900

    Revert "fix: 3.2.4-2 예제 코드 오류 수정"
    This reverts commit 2104b332af71ff0e05a4af4b755b04ac0567a615.

--- 3장/3.2.4-2.ts
+++ 3장/3.2.4-2.ts
@@ -1,8 +1,9 @@
 const PromotionList = [
   { type: "product", name: "chicken" },
   { type: "product", name: "pizza" },
-  { type: "card", name: "cheer-up" },
+  { type: "card", name: "chee-up" },
 ];
 
+type ElementOf<T> = (typeof T)[number];
 // type PromotionItemType = { type: string; name: string }
-type PromotionItemType = typeof PromotionList[number];
+type PromotionItemType = ElementOf<PromotionList>;
